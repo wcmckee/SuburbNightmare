2bfbf532726475c2ddfe745d40240de42f43fbb1
artctrl <will@artcontrol.me>
2013-07-14 04:52:48 +1200

    girl texture & animation

--- Assets/scripts/LoadMenu.js
+++ Assets/scripts/LoadMenu.js
@@ -1,7 +1,7 @@
 function OnGUI () {
 
 	// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-	if (GUI.Button (Rect (300,200,156,56), "Menu")) {
+	if (GUI.Button (Rect (48,24,48,24), "Menu")) {
 		Application.LoadLevel (0);
 	}
 }
---
+++ Assets/scripts/SceneTimer.js
@@ -0,0 +1,4 @@
+function Start() {
+    yield WaitForSeconds(15);
+    Application.LoadLevel(0);
+}
